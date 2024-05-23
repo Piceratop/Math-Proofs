79fac68b292b910c2230889cbef353ec2632da46
buinhatminh2k52005 <buinhatminh2k52005@gmail.com>
2024-05-23 08:41:29 +0000

    Add assign command

--- math_error.py
+++
@@ -1,7 +0,0 @@
-class CommandLengthException(Exception):
-    def __init__(self, command, length):
-        self.command = command
-        self.length = length
-
-    def __str__(self):
-        return f"Command {self.command} has length {self.length}"
--- math_interpreter.py
+++ math_interpreter.py
@@ -1,11 +1,41 @@
-from math_error import *
+math_variables = dict()
+
+def check_command_length(command, min_length, max_length, given):
+    if given < min_length or given > max_length:
+        print(f"Invalid number of arguments for {command}, expected between {min_length} and {max_length}, got {given}")
+        exit(1)
+
+def check_unknown_variable(name):
+    if name not in math_variables:
+        print(f"'{name}' is not defined")
+        exit(1)
+
+def unknown_command(command):
+    print(f"Unknown command: {command}")
+    exit(1)
 
 with open("test.mthc", "r") as f:
-    code = f.read().strip().split()
-    # The "set" command
-    if code[0] == "set":
-        code = code[1:]
-        if len(code) == 0:
-            raise CommandLengthException('set', 1)
-    else:
-        pass
+    for code_line in f.readlines():
+        code = code_line.strip().split(maxsplit=-1)
+        # "assign" command
+        if code[0] == "assign":
+            code = code[1:]
+            check_command_length("assign", 2, float('inf'), len(code))
+            var_name = code[0]
+            check_unknown_variable(var_name)
+            math_variables[var_name]["value"] = set()
+            for value in code[1:]:
+                math_variables[var_name]["value"].add(value)
+
+        # "set" command
+        elif code[0] == "set":
+            code = code[1:]
+            check_command_length("set", 1, 1, len(code))
+            math_variables[code[0]] = {
+                "value": set(),
+                "type": "set"
+            }
+        else:
+            unknown_command(code[0])
+
+print(math_variables)
