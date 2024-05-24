b21a32434bac0dbd8dc3bc1f78cc13e05869f41c
buinhatminh2k52005 <buinhatminh2k52005@gmail.com>
2024-05-24 08:56:20 +0000

    Refactor basic syntax

--- math_interpreter.py
+++ math_interpreter.py
@@ -1,43 +1,45 @@
 math_variables = dict()
+keywords = {"=", "let", "be"}
 
-def check_command_length(command, min_length, max_length, given):
-    if given < min_length or given > max_length:
-        print(f"Invalid number of arguments for {command}, expected between {min_length} and {max_length}, got {given}")
+def validate_minimum(tokens, keyword, required):
+    if len(tokens) < required:
+        print(f"Syntax error: ' {keyword} ' requires at least {required - 1} arguments.")
         exit(1)
 
-def check_unknown_variable(name):
-    if name not in math_variables:
-        print(f"'{name}' is not defined")
+def validate_nonkeyword(token):
+    if token in keywords:
+        print(f"Syntax error: ' {token} ' is a keyword.")
         exit(1)
 
-def unknown_command(command):
-    print(f"Unknown command: {command}")
-    exit(1)
+def validate_required_keyword(token, keyword):
+    if token != keyword:
+        print(f"Syntax error: ' {keyword} ' was expected, but got ' {token} ' instead.")
+        exit(1)
+
 
 with open("test.mthc", "r") as f:
-    for code_line in f.readlines():
-        code = code_line.strip().split(maxsplit=-1)
-        # "assign" command
-        if code[0] == "assign":
-            code = code[1:]
-            check_command_length("assign", 2, float('inf'), len(code))
-            var_name = code[0]
-            check_unknown_variable(var_name)
-            var_type = math_variables[var_name]["type"]
-            if var_type == "set":
-                math_variables[var_name]["value"] = set()
-                for value in code[1:]:
-                    math_variables[var_name]["value"].add(value)
+    code_lines = f.readlines()
+    print(code_lines)
+    for code in code_lines:
+        tokens = code.strip().split(maxsplit=-1)
+        if tokens[0] == "let":
+            if len(tokens) == 2:
+                validate_nonkeyword(tokens[1])
+                math_variables[tokens[1]] = {
+                    "type": "any",
+                    "value": None
+                }
+            elif len(token) < 2:
+                print(f"Syntax error:' {tokens[0]} ' was expected, but got ' {tokens[1]} ' instead.")
+                exit(1)
+            else:
+                validate_nonkeyword(tokens[1])
+                validate_required_keyword(tokens[2], "be")
+                validate_nonkeyword(tokens[3])
+                math_variables[tokens[1]] = {
+                    "type": tokens[3],
+                    "value": None
+                }
 
-        # "set" command
-        elif code[0] == "set":
-            code = code[1:]
-            check_command_length("set", 1, 1, len(code))
-            math_variables[code[0]] = {
-                "value": set(),
-                "type": "set"
-            }
-        else:
-            unknown_command(code[0])
 
-print(math_variables)
+# print(math_variables)
