21116d18a39097a87f271932b7d340e6fadb2e24
buinhatminh_hsgs20 <buinhatminh_hsgs20@hus.edu.vn>
2024-09-13 04:12:45 +0000

    Add parser

---
+++ math_interpreter.py
@@ -0,0 +1,23 @@
+def parse_tokens(line):
+    tokens_list = []
+    current_token = ""
+    for ch in line:
+        if ch.isspace():
+            if current_token:
+                tokens_list.append(current_token)
+                current_token = ""
+        elif ch == "{" or ch == "}":
+            if current_token:
+                tokens_list.append(current_token)
+                current_token = ""
+            tokens_list.append(ch)
+        else:
+            current_token += ch
+    if current_token:
+        tokens_list.append(current_token)
+    return tokens_list
+
+with open("test.mthc", "r") as f:
+    for line in f:
+        line = parse_tokens(line)
+        print(line)
